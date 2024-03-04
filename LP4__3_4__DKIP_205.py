75793f4d6403f832d1d5d875bd6a6fef2c81941c
bahti <bahti@DESKTOP-63B4UVC>
2024-03-04 10:12:28 +0300

    Добавьте файлы проекта.

---
+++ LP4__3_4__DKIP_205.py
@@ -0,0 +1,22 @@
+﻿# Лабораторная работа №4: Изучение интегрированной среды разработчика
+# Выполняли Бахтин Данила, Бритвин Антон
+# ДКИП-205
+
+def print_hex_multiplication_table(n):
+    print("Hex Multiplication Table (up to {0} x {0}):".format(n))
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            result = hex(i * j)[2:].upper()  # Получаем результат умножения и конвертируем в шестнадцатеричное число
+            print(f"{hex(i)[2:]} x {hex(j)[2:]} = {result}")
+
+def print_hex_addition_table(n):
+    print("Hex Addition Table (up to {0} + {0}):".format(n))
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            result = hex(i + j)[2:].upper()  # Получаем результат сложения и конвертируем в шестнадцатеричное число
+            print(f"{hex(i)[2:]} + {hex(j)[2:]} = {result}")
+
+n = 16  # Максимальное число для таблицы
+print_hex_multiplication_table(n)
+print()
+print_hex_addition_table(n)
